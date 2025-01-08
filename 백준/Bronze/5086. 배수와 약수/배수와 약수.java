e8d5e5e868b653bc5097709db1896c4507a5fef5
김대호0513 <kimdaeho0513@gmail.com>
2025-01-08 15:32:58 +0700

    [Bronze III] Title: 배수와 약수, Time: 96 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,23 +1,29 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int a,b;
-        do{
-            a = sc.nextInt();
-            b = sc.nextInt();
-            String s="";
-            if(a!=0&&b!=0){
-                if(b%a==0){
-                    s = "factor";
-                } else if(a%b==0){
-                    s = "multiple";
-                } else {
-                    s = "neither";
-                }
-                System.out.println(s);
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        while(true){
+            String[] s = br.readLine().split(" ");
+            int a = Integer.parseInt(s[0]);
+            int b = Integer.parseInt(s[1]);
+            if(a==0&&b==0){
+                break;
             }
-        } while(a!=0&&b!=0);
+            if(b%a==0){
+                sb.append("factor");
+            } else if(a%b==0){
+                sb.append("multiple");
+            } else {
+                sb.append("neither");
+            }
+            sb.append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
